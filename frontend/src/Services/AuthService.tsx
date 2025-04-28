3e9e40a7daffaf43994d6c15f861e64b247c4315
Duyvd04 <vuducduy1112004@gmail.com>
2025-04-28 09:27:35 +0700

    frontend

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -1,9 +1,9 @@
-import axios from 'axios';
-const base_url = `${process.env.REACT_APP_API_URL}/auth/`;
-const loginUser = async (login:any)=> {
-    return axios.post(`${base_url}login`, login)
-        .then((result:any) => result.data)
-        .catch((error:any) =>{throw error;});
+import axiosInstance from "../Interceptor/AxiosInterceptor";
+
+const loginUser = async (login: { email: string; password: string }) => {
+    return axiosInstance.post(`/auth/login`, login)
+        .then((result: any) => result.data)
+        .catch((error: any) => { throw error; });
 }
 
-export {loginUser};
+export { loginUser };
