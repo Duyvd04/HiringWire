b9d2d85f7214d956920837a779df53345370a791
Duyvd04 <vuducduy1112004@gmail.com>
2025-04-28 09:11:35 +0700

    frontend

--- frontend/src/Interceptor/AxiosInterceptor.tsx
+++ frontend/src/Interceptor/AxiosInterceptor.tsx
@@ -4,7 +4,7 @@ import { removeJwt } from "../Slices/JwtSlice";
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:8080'
-    baseURL: 'https://hiringwire-production.up.railway.app'
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 axiosInstance.interceptors.request.use(
